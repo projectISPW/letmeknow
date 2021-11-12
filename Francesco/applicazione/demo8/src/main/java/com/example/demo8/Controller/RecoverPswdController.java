c93d1e12e08bc2f49c255c5f368c3a8164332d4e
Frenk202 <francescodamata@gmail.com>
2021-11-12 01:23:23 +0100

    creato profetto ufficiale di let me know, errore nel passaggio da frame di login a frame del recover password no risolto

---
+++ Francesco/applicazione/demo8/src/main/java/com/example/demo8/Controller/RecoverPswdController.java
@@ -0,0 +1,27 @@
+package com.example.demo8;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignupController
+{
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    protected void switchToScene1(ActionEvent event) throws IOException {
+        root= FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
