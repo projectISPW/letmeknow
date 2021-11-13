2a564456c5d9dfa79481e0192670a5f9a8ff1c3d
Frenk202 <francescodamata@gmail.com>
2021-11-13 19:36:16 +0100

    fatta homepage con navbar

--- Francesco/applicazione/demo8/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demo8 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.demo8 to javafx.fxml;
-    exports com.example.demo8;
-}
---
+++ Francesco/letMeKnow/letMeKnow/src/main/java/com/example/letmeknow/homePageController.java
@@ -0,0 +1,4 @@
+package com.example.letmeknow;
+
+public class homePageController {
+}
