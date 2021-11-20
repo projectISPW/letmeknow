63a7c00cfbcaea79036d92080066955c3d67ae30
EduardISPW <edward.olenic99@gmail.com>
2021-11-20 18:12:24 +0100

    Rimosso cartella doppione

--- Francesco/letMeKnow/letMeKnow/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.letmeknow {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.letmeknow to javafx.fxml;
-    exports com.example.letmeknow;
-}
--- Francesco/letMeKnow/src/main/java/module-info.java
+++ Francesco/letMeKnow/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.letmeknow {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.letmeknow to javafx.fxml;
