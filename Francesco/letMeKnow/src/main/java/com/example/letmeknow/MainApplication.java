978118f1bc04126987cb7f8752f1cab823da6a77
EduardISPW <edward.olenic99@gmail.com>
2021-11-22 18:46:03 +0100

    Aggiunto Alert Confirmation su setOnCloseRequest all'uscita dall'app

--- Francesco/letMeKnow/src/main/java/com/example/letmeknow/MainApplication.java
+++ Francesco/letMeKnow/src/main/java/com/example/letmeknow/MainApplication.java
@@ -1,11 +1,16 @@
 package com.example.letmeknow;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -16,9 +21,35 @@ public class MainApplication extends Application {
         stage.setScene(new Scene(root));
         stage.setTitle("Login");
         stage.setResizable(false);
+
+
+        //Alert in fase di uscita dall'applicazione
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Exit Confirmation");
+                alert.setHeaderText("Are you sure to exit program?");
+                alert.setContentText("If you want to exit you'll be logged out from application.");
+
+                if(alert.showAndWait().get() == ButtonType.OK){
+                    //Log Out Account and Exit Program
+                    // ...
+                    System.out.println("Prompt: On Log Out phase");
+                    Platform.exit();
+                    System.exit(0);
+                }else{
+                    System.out.println("Prompt: On Cancel Confirmation Alert. Consume Event");
+                    windowEvent.consume();
+                }
+            }
+        });
+
         stage.show();
     }
-    //alert in fase di uscita dall'app
+
+
+
 
 
     public static void main(String[] args) {
