11148a994f36835a365ecf490f8d47b77d07e66d
Frenk202 <francescodamata@gmail.com>
2021-11-23 10:16:35 +0100

    cose

---
+++ letMeKnow/letMeKnow/src/main/java/com/example/letmeknow/MainApplication.java
@@ -0,0 +1,31 @@
+package com.example.letmeknow;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        /*
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+         */
+        Parent root=FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Login");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+                launch();
+    }
+}
